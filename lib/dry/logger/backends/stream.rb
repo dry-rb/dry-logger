dbcb67c503fd36a1ecd09f1062da1f0ee057ed75
Piotr Solnica <piotr.solnica@gmail.com>
2021-04-17 00:07:44 +0200

    Add yard to Stream

--- lib/dry/logger/backends/stream.rb
+++ lib/dry/logger/backends/stream.rb
@@ -9,10 +9,16 @@ module Dry
   module Logger
     module Backends
       class Stream < ::Logger
+        # @since 0.1.0
+        # @api private
         attr_reader :stream
 
+        # @since 0.1.0
+        # @api private
         attr_reader :level
 
+        # @since 0.1.0
+        # @api private
         def initialize(stream:, level: INFO, formatter: nil, **)
           super(stream)
 
