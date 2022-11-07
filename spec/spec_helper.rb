9ebe3b8f9ee715e1364741a874e910f01dfca957
Peter Solnica <peter@solnica.online>
2022-11-07 22:00:25 +0100

    Use standard rspec options from the template gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,28 +18,7 @@ FileUtils.mkdir_p(RELATIVE_TMP)
 
 TMP = SPEC_ROOT.join("..", RELATIVE_TMP).realpath
 
-RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
-
-  config.shared_context_metadata_behavior = :apply_to_host_groups
-
-  config.filter_run_when_matching :focus
-  config.disable_monkey_patching!
-
-  config.warnings = true
-
-  config.default_formatter = "doc" if config.files_to_run.one?
-  config.profile_examples = 10
-
-  config.order = :random
-  Kernel.srand config.seed
-end
+require_relative "support/rspec_options"
 
 require "dry/logger"
 
