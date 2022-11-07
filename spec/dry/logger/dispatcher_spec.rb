d9a5082043edc1cca2495c230d43c32a94a20ede
Peter Solnica <peter@solnica.online>
2022-11-07 08:47:44 +0100

    Add a spec for Dispatcher#close

--- spec/dry/logger/dispatcher_spec.rb
+++ spec/dry/logger/dispatcher_spec.rb
@@ -3,14 +3,32 @@
 require "dry/logger/dispatcher"
 
 RSpec.describe Dry::Logger::Dispatcher do
-  subject(:logger) { Dry.Logger(:test) }
-
   describe "#add_backend" do
+    subject(:logger) { Dry.Logger(:test) }
+
     it "adds a new backend" do
       logger.add_backend(stream: SPEC_ROOT.join("../log/test.log"))
 
       expect(logger.backends.size).to be(2)
       expect(logger.backends.last).to be_instance_of(Dry::Logger::Backends::File)
+    end
+  end
+
+  describe "#close" do
+    subject(:logger) { Dry.Logger(:test) }
+
+    let(:backend_one) { instance_spy(Dry::Logger::Backends::Stream) }
+    let(:backend_two) { instance_spy(Dry::Logger::Backends::Stream) }
+
+    before do
+      logger.add_backend(backend_one).add_backend(backend_two)
+    end
+
+    it "closes all backends" do
+      expect(logger.close).to be(true)
+
+      expect(backend_one).to have_received(:close)
+      expect(backend_two).to have_received(:close)
     end
   end
 end
