fd31a56c7d2af46fe27527b0f99ed3483e9ea42c
Peter Solnica <peter@solnica.online>
2022-11-12 06:28:33 +0100

    Use stream context in the string specs

--- spec/dry/logger/formatters/string_spec.rb
+++ spec/dry/logger/formatters/string_spec.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe Dry::Logger::Formatters::String do
+  include_context "stream"
+
   subject(:logger) do
-    Dry.Logger(:test, template: "[%<progname>s] [%<severity>s] [%<time>s] %<message>s")
+    Dry.Logger(:test, stream: stream, template: "[%<progname>s] [%<severity>s] [%<time>s] %<message>s")
   end
 
   before do
@@ -11,17 +13,13 @@ RSpec.describe Dry::Logger::Formatters::String do
 
   describe "using customized template" do
     it "when passed as a symbol, it has key=value format for string messages" do
-      output = with_captured_stdout do
-        logger.info("foo")
-      end
+      logger.info("foo")
 
       expect(output).to eq "[test] [INFO] [2017-01-15 16:00:23 +0100] foo\n"
     end
 
     it "has key=value format for hash messages" do
-      output = with_captured_stdout do
-        logger.info(foo: "bar")
-      end
+      logger.info(foo: "bar")
 
       expect(output).to eq %([test] [INFO] [2017-01-15 16:00:23 +0100] foo="bar"\n)
     end
@@ -30,9 +28,7 @@ RSpec.describe Dry::Logger::Formatters::String do
       backtrace = ["file-1.rb:312", "file-2.rb:12", "file-3.rb:115"]
       exception = StandardError.new("foo").tap { |e| e.set_backtrace(backtrace) }
 
-      output = with_captured_stdout do
-        logger.error(exception)
-      end
+      logger.error(exception)
 
       expected = <<~STR
         [test] [ERROR] [2017-01-15 16:00:23 +0100] StandardError: foo
