dfa513c64b49891eba1135d75d81ef7affb4d30e
Peter Solnica <peter@solnica.online>
2022-11-14 06:19:00 +0100

    Clean up #close spec

--- spec/dry/logger/dispatcher/close_spec.rb
+++ spec/dry/logger/dispatcher/close_spec.rb
@@ -15,15 +15,7 @@ RSpec.describe Dry::Logger::Dispatcher, "#close" do
     end
   end
 
-  context "when stream is $stdout" do
-    let(:stream) { $stdout }
-
-    it "does not close stream" do
-      expect { print "in $stdout" }.to output("in $stdout").to_stdout
-    end
-  end
-
-  context "when file" do
+  context "when stream is a file" do
     let(:stream) { random_file_name }
 
     it "closes stream" do
@@ -33,7 +25,7 @@ RSpec.describe Dry::Logger::Dispatcher, "#close" do
     end
   end
 
-  context "when StringIO" do
+  context "when stream is StringIO" do
     let(:stream) { StringIO.new }
 
     it "closes stream" do
@@ -43,7 +35,7 @@ RSpec.describe Dry::Logger::Dispatcher, "#close" do
     end
   end
 
-  context "when IO" do
+  context "when stream is IO" do
     let(:stream) { io_stream(destination) }
     let(:destination) { file_with_directory }
 
