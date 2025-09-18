75c4611d29928374256517019c3d5c2ea3bd34c1
Petrik <petrik@deheus.net>
2025-09-18 19:53:22 +0200

    Support passing payloads in blocks

--- spec/dry/logger_spec.rb
+++ spec/dry/logger_spec.rb
@@ -34,6 +34,14 @@ RSpec.describe Dry::Logger do
       expect(output).to match(message)
     end
 
+    it "logs to $stdout by default using a payload block" do
+      message = "hello, world"
+
+      logger.info { { test: true } }
+
+      expect(output).to match("test=true")
+    end
+
     it "logs to $stdout by default using a plain text block message and payload" do
       message = "hello, world"
 
@@ -41,6 +49,15 @@ RSpec.describe Dry::Logger do
 
       expect(output).to match("#{message} test=true")
     end
+
+    it "logs to $stdout by default using a plain text message and payload block" do
+      message = "hello, world"
+
+      logger.info(message) { { test: true } }
+
+      expect(output).to match("#{message} test=true")
+    end
+
   end
 
   context "using progname" do
